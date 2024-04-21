1483e6134b9b21c6a7e962cb2bc26e8637aad361
contrerasv742 <contrerasv742@gmail.com>
2024-04-21 13:43:01 -0700

    Validate Function Comments

--- scripts/cpp/src/main.cpp
+++ scripts/cpp/src/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
-int main() {
-  // Tests Number: 1789372997
-  std::string account_number = "1789372997";
-
+/* @brief Implementation of Lunh's law
+ * @params account_number is the account we want to validate
+ * @return full acount number if valid
+ * */
+std::string validate_account(std::string account_number) {
   int count = 0;
   for (int i = account_number.size() - 1; i >= 0; i--) {
     int digit = account_number[i] - '0';
@@ -12,17 +13,21 @@ int main() {
       int doubled = digit * 2;
       int sum = (doubled / 10) + (doubled % 10);
       count += sum;
-
-      // std::cout << sum << std::endl;
     } else {
       count += digit;
-
-      // std::cout << digit << std::endl;
     }
   }
+  int last_num = 10 - (count % 10); 
+  return account_number + std::to_string(last_num);   
+}
 
-  std::cout << "Full Account: " << account_number;
-  std::cout << 10 - (count % 10) << std::endl;
+int main() {
+  // Tests Number: 1789372997
+  std::string account_number = "1789372997";
+
+  std::string full_account = validate_account(account_number); 
+
+  std::cout << "Full Account: " << full_account << "\n";
 
   return 0;
 }
